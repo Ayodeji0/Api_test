1a42b2a440886194427ee2c6d17f971c3c7c934f
Ayodeji <interpercelservice@gmail.com>
2024-04-29 21:40:27 +0100

    CURRENCY LENGTH COUNTED WITHIN RESPONSE

--- apitask/apitest.spec.ts
+++ apitask/apitest.spec.ts
@@ -1,10 +1,15 @@
 import { test, expect } from "@playwright/test";
 
-const Url = "https://v6.exchangerate-api.com/v6/1fc80820c72b0163bc9c7536/latest/USD"
+const Url =
+  "https://v6.exchangerate-api.com/v6/1fc80820c72b0163bc9c7536/latest/USD";
 
-test('Automate ExchangeRate API Test', async ({request})=>{
- const response = await request.get(Url);
-//  Verifying HTTP STATUS TO BE 200
-expect(response.status()).toBe(200);
-
-})
+test("Automate ExchangeRate API Test", async ({ request }) => {
+  const response = await request.get(Url);
+  //  Verifying HTTP STATUS TO BE 200
+  expect(response.status()).toBe(200);
+  const responseData = await response.json();
+  console.log(responseData);
+  /* NUMBERS OF CURRENCY RETURNED WITHIN THE RESPONSE, object.keys is used because the currencies are within an object not arrays */
+  const dataLength = Object.keys(responseData.conversion_rates).length;
+  console.log(dataLength);
+});

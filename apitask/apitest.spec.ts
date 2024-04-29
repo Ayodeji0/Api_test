fbe5e271d0cb8de2f133b3e9e516ec3b6b119474
Ayodeji <interpercelservice@gmail.com>
2024-04-29 22:05:05 +0100

    CODE REFACTORED TO VERIFY GBP IN RESPONSE, API TEST DONE AND SUCCESSFUL

--- apitask/apitest.spec.ts
+++ apitask/apitest.spec.ts
@@ -1,15 +1,17 @@
 import { test, expect } from "@playwright/test";
 
-const Url =
-  "https://v6.exchangerate-api.com/v6/1fc80820c72b0163bc9c7536/latest/USD";
+const Url ="https://v6.exchangerate-api.com/v6/1fc80820c72b0163bc9c7536/latest/USD";
 
 test("Automate ExchangeRate API Test", async ({ request }) => {
   const response = await request.get(Url);
-  //  Verifying HTTP STATUS TO BE 200
+  //  VERIFY HTTP STATUS TO BE 200
   expect(response.status()).toBe(200);
   const responseData = await response.json();
   console.log(responseData);
-  /* NUMBERS OF CURRENCY RETURNED WITHIN THE RESPONSE, object.keys is used because the currencies are within an object not arrays */
-  const dataLength = Object.keys(responseData.conversion_rates).length;
-  console.log(dataLength);
+  /* EXTRACT CURRENCIES FROM RESPONSE FROM API USING OBJECT.KEYS METHOD SINCE RESPONSE DOESN'T RETURN ARRAYS */
+ const currencies = Object.keys(responseData.conversion_rates)
+  const currenciesTotal = currencies.length
+  console.log(currenciesTotal);
+//   VERIFY CURRENCY 'GBP' IS SHOWN WITHIN THE RESPONSE
+   expect(currencies).toContain('GBP')
 });
